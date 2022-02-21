871175ddcac5d858aacd075c253f879ef907b2f7
linnefromice <linnefromice@gmail.com>
2022-02-21 12:36:08 +0900

    fix - code formats

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
   );
