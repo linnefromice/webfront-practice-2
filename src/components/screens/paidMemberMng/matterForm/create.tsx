c267869e7e677ebd4a0ecdc704e6ba0ba000c95b
linnefromice <linnefromice@gmail.com>
2022-02-28 04:09:24 +0900

    add screen

---
+++ src/components/screens/paidMemberMng/matterForm/create.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { VFC } from "react";
+
+export const CreateScreen: VFC = () => {
+  return (
+    <>
+      <Head>
+        <title>案件情報フォーム</title>
+      </Head>
+      <h1>案件情報フォーム</h1>
+    </>
+  );
+};
---
+++ src/pages/paidMemberMng/matterForm/create.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+import { CreateScreen } from "../../../components/screens/paidMemberMng/matterForm/create";
+
+const PaidMembersMngMatterFormCreate: NextPage = () => <CreateScreen />;
+
+export default PaidMembersMngMatterFormCreate;
