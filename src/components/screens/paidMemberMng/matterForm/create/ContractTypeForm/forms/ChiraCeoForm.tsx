18fcbf9d018df088b10ee0700d80638ad505595f
linnefromice <linnefromice@gmail.com>
2022-03-06 01:07:22 +0900

    add base of ContractTypeForm (for ChiraCeo)

--- src/components/screens/paidMemberMng/matterForm/create/ContractTypeForm/forms/ChiraCeoForm.tsx
+++ src/components/screens/paidMemberMng/matterForm/create/ContractTypeForm/forms/ChiraCeoForm.tsx
@@ -1,4 +1,78 @@
 import { Typography } from "@mui/material";
 import { VFC } from "react";
+import { TextField } from "../../../../../../uiParts/TextField";
 
-export const ChiraCeoForm: VFC = () => <Typography>チラCEO</Typography>;
+export const ChiraCeoForm: VFC = () => {
+  return (
+    <>
+      <Typography variant="h5" sx={{ marginBottom: 2 }}>
+        {`チラCEO 新規`}
+      </Typography>
+      <TextField
+        formDataKey="companyName"
+        rules={{ required: "入力必須パラメータです" }}
+        label="企業名"
+      />
+      <TextField
+        formDataKey="url"
+        rules={{ required: "入力必須パラメータです" }}
+        label="URL"
+      />
+      <TextField
+        formDataKey="employeeSize"
+        rules={{ required: "入力必須パラメータです" }}
+        label="従業員規模"
+      />
+      <TextField formDataKey="postCode" label="郵便番号" />
+      <TextField
+        formDataKey="address"
+        rules={{ required: "入力必須パラメータです" }}
+        label="住所"
+      />
+      <TextField
+        formDataKey="telNumberCompany"
+        rules={{ required: "入力必須パラメータです" }}
+        label="電話番号(会社)"
+      />
+      <TextField
+        formDataKey="telNumberManagerPhone"
+        label="電話番号(担当者携帯)"
+      />
+      <TextField
+        formDataKey="managerName"
+        rules={{ required: "入力必須パラメータです" }}
+        label="担当者名"
+      />
+      <TextField formDataKey="managerRole" label="担当者役職" />
+      <TextField
+        formDataKey="managerMailAddress"
+        rules={{ required: "入力必須パラメータです" }}
+        label="担当者メールアドレス"
+      />
+      <TextField
+        formDataKey="accountingRoleName"
+        rules={{ required: "入力必須パラメータです" }}
+        label="先方経理担当者名"
+      />
+      <TextField
+        formDataKey="invoiceShippingMailAddress"
+        rules={{ required: "入力必須パラメータです" }}
+        label="請求書送付先メールアドレス"
+      />
+      <TextField formDataKey="contractPlan" label="契約プラン" />
+      <TextField
+        formDataKey="initialCost"
+        rules={{ required: "入力必須パラメータです" }}
+        label="初期費用(税抜)"
+      />
+      <TextField
+        formDataKey="monthlyAmount"
+        rules={{ required: "入力必須パラメータです" }}
+        label="月額(税抜)"
+      />
+      <TextField formDataKey="firstConsultingDay" label="初回コンサル日" />
+      <TextField formDataKey="contractStartDate" label="契約開始日" />
+      <TextField formDataKey="contractEndDate" label="契約終了日" />
+    </>
+  );
+};
--- src/components/screens/paidMemberMng/matterForm/create/ContractTypeForm/types.ts
+++ src/components/screens/paidMemberMng/matterForm/create/ContractTypeForm/types.ts
@@ -13,10 +13,37 @@ type CommonFormData = {
   otherPaymentMethod?: string;
 };
 
-export type ChiraCeoFormData = CommonFormData & {};
+export const ContractPlan = {
+  Light: "ライトプラン",
+  Basic: "ベーシックプラン",
+  Premium: "プレミアムプラン",
+  MajorDevelopment: "大手開拓プラン",
+} as const;
+type ContractPlanKeyType = keyof typeof ContractPlan;
+export type ChiraCeoFormData = CommonFormData & {
+  companyName: string; // 企業名
+  url: string; // URL
+  employeeSize: string; // 従業員規模
+  postCode: string; // 郵便番号
+  address: string; // 住所
+  telNumberCompany: string; // 電話番号(会社)
+  telNumberManagerPhone: string; // 電話番号(担当者携帯)
+  managerName: string; // 担当者名
+  managerRole: string; // 担当者役職
+  managerMailAddress: string; // 担当者メールアドレス
+  accountingRoleName: string; // 先方経理担当者名
+  invoiceShippingMailAddress: string; // 請求書送付先メールアドレス
+  contractPlan: ContractPlanKeyType; // 契約プラン
+  initialCost: string; // 初期費用(税抜)
+  monthlyAmount: string; // 月額(税抜)
+  firstConsultingDay: string; // 初回コンサル日
+  contractStartDate: string; // 契約開始日
+  contractEndDate: string; // 契約終了日
+};
 export type JoiningAgencyFormData = CommonFormData & {};
 export type LetterMeasuresAndChiraCeoFormData = CommonFormData & {};
 export type LetterGetFormData = CommonFormData & {};
+
 export type FormData =
   | ChiraCeoFormData
   | JoiningAgencyFormData
