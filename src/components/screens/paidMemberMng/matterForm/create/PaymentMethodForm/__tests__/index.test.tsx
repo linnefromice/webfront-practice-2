c89ef9bb6caa4a9c2c2f125fd2346980057ea360
linnefromice <linnefromice@gmail.com>
2022-03-06 15:41:02 +0900

    refactor

--- src/components/screens/paidMemberMng/matterForm/create/PaymentMethodForm/__tests__/index.test.tsx
+++ src/components/screens/paidMemberMng/matterForm/create/PaymentMethodForm/__tests__/index.test.tsx
@@ -3,6 +3,7 @@ import { composeStories } from "@storybook/testing-react";
 import { render, RenderResult, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Contents } from "..";
+import { PaymentMethodKeyType } from "../../types";
 import * as stories from "../PaymentMethodForm.stories";
 
 const checkCommonItems = async (screen: RenderResult) => {
@@ -17,6 +18,28 @@ const checkCommonItems = async (screen: RenderResult) => {
       name: "次ページに遷移",
     })
   ).toBeInTheDocument();
+};
+
+const checkToSubmitFailed = async (paymentMethod: PaymentMethodKeyType) => {
+  const onSubmit = jest.fn();
+  const onError = jest.fn();
+  const screen = render(
+    <ThemeProvider theme={createTheme()}>
+      <Contents
+        onSubmit={onSubmit}
+        onError={onError}
+        backPage={() => {}}
+        paymentMethod={paymentMethod}
+      />
+    </ThemeProvider>
+  ); // TODO: remove ThemeProvider
+  await userEvent.click(
+    screen.getByRole("button", {
+      name: "次ページに遷移",
+    })
+  );
+  await waitFor(() => expect(onSubmit).not.toHaveBeenCalled());
+  await waitFor(() => expect(onError).toHaveBeenCalledTimes(1));
 };
 
 describe("src/components/screens/paidMemberMng/matterForm/create/PaymentMethodForm", () => {
@@ -62,28 +85,7 @@ describe("src/components/screens/paidMemberMng/matterForm/create/PaymentMethodFo
         await waitFor(() => expect(onError).not.toHaveBeenCalled());
       });
 
-      test("failure", async () => {
-        const onSubmit = jest.fn();
-        const onError = jest.fn();
-        const screen = render(
-          <ThemeProvider theme={createTheme()}>
-            <Contents
-              onSubmit={onSubmit}
-              onError={onError}
-              backPage={() => {}}
-              paymentMethod={"CreditCard"}
-            />
-          </ThemeProvider>
-        ); // TODO: remove ThemeProvider
-
-        await userEvent.click(
-          screen.getByRole("button", {
-            name: "次ページに遷移",
-          })
-        );
-        await waitFor(() => expect(onSubmit).not.toHaveBeenCalled());
-        await waitFor(() => expect(onError).toHaveBeenCalledTimes(1));
-      });
+      test("failure", async () => await checkToSubmitFailed("CreditCard"));
     });
   });
 });
