ad55876e51a08939def114cd97cc96db3517f5a1
linnefromice <linnefromice@gmail.com>
2022-03-08 00:25:13 +0900

    add labels for SelectContractTypeForm

--- src/components/screens/paidMemberMng/matterForm/create/SelectContractTypeForm/types.ts
+++ src/components/screens/paidMemberMng/matterForm/create/SelectContractTypeForm/types.ts
@@ -3,3 +3,7 @@ import { SelectContractKeyType } from "../types";
 export type FormData = {
   selectContractType: SelectContractKeyType;
 };
+
+export const FormDataLabels: { [key in keyof FormData]: string } = {
+  selectContractType: "新規案件種別",
+};
