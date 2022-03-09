006f364e989565f8393cc04e704273e359ffdbbd
linnefromice <linnefromice@gmail.com>
2022-03-09 12:03:54 +0900

    refactor confirmation view - create list

--- src/components/screens/paidMemberMng/matterForm/create/Confirmation/index.tsx
+++ src/components/screens/paidMemberMng/matterForm/create/Confirmation/index.tsx
@@ -7,7 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import { Button } from "components/uiParts";
-import { VFC } from "react";
+import { Fragment, VFC } from "react";
 import { ChiraCeoFormDataLabels as ContractTypeFormDataLabels } from "../ContractTypeForm/types";
 import { FormDataLabels as FirstFormDataLabels } from "../FirstForm/types";
 import { FormDataLabels as OnboardingFormDataLabels } from "../OnboardingForm/types";
@@ -30,12 +30,13 @@ const ListItem: VFC<{ label: string; value: string }> = ({ label, value }) => (
   />
 );
 
-const List: VFC<{
+type ListProps = {
   keyPrefix: string;
   label: string;
   formLabels: { [key: string]: string };
   form: { [key: string]: any };
-}> = ({ keyPrefix, label, formLabels, form }) => (
+};
+const List: VFC<ListProps> = ({ keyPrefix, label, formLabels, form }) => (
   <MuiList subheader={label}>
     {Object.keys(form).map((v) => (
       <ListItem
@@ -51,6 +52,39 @@ type Props = Omit<MatterFormInfo, "currentPage"> & {
   backPage: () => void;
 };
 const Contents: VFC<Props> = (props) => {
+  const formsListProps: ListProps[] = [
+    {
+      keyPrefix: "firstForm",
+      label: "基礎情報(ページ1)",
+      formLabels: FirstFormDataLabels,
+      form: props.firstFormData as { [key: string]: any },
+    },
+    {
+      keyPrefix: "selectContractTypeForm",
+      label: "案件種別選択(ページ2)",
+      formLabels: SelectContractTypeFormDataLabels,
+      form: props.selectContractTypeFormData as { [key: string]: any },
+    },
+    {
+      keyPrefix: "contractTypeForm",
+      label: "案件種別毎入力項目(ページ3)",
+      formLabels: ContractTypeFormDataLabels,
+      form: props.contractTypeFormData as { [key: string]: any },
+    },
+    {
+      keyPrefix: "paymentMethodForm",
+      label: "支払方法(ページ4)",
+      formLabels: PaymentMethodFormDataLabels,
+      form: props.paymentMethodFormData as { [key: string]: any },
+    },
+    {
+      keyPrefix: "onboardingForm",
+      label: "オンボーディング情報(ページ5)",
+      formLabels: OnboardingFormDataLabels,
+      form: props.onBoardingFormData as { [key: string]: any },
+    },
+  ];
+
   return (
     <>
       <Typography variant="h5" sx={{ marginBottom: 2 }}>
@@ -64,34 +98,12 @@ const Contents: VFC<Props> = (props) => {
         form={props.firstFormData as { [key: string]: any }}
       />
       <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
-      <List
-        keyPrefix="selectContractTypeForm"
-        label="案件種別選択(ページ2)"
-        formLabels={SelectContractTypeFormDataLabels}
-        form={props.selectContractTypeFormData as { [key: string]: any }}
-      />
-      <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
-      <List
-        keyPrefix="contractTypeForm"
-        label="案件種別毎入力項目(ページ3)"
-        formLabels={ContractTypeFormDataLabels}
-        form={props.contractTypeFormData as { [key: string]: any }}
-      />
-      <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
-      <List
-        keyPrefix="paymentMethodForm"
-        label="支払方法(ページ4)"
-        formLabels={PaymentMethodFormDataLabels}
-        form={props.paymentMethodFormData as { [key: string]: any }}
-      />
-      <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
-      <List
-        keyPrefix="onboardingForm"
-        label="オンボーディング情報(ページ5)"
-        formLabels={OnboardingFormDataLabels}
-        form={props.onBoardingFormData as { [key: string]: any }}
-      />
-      <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
+      {formsListProps.map((v: ListProps) => (
+        <Fragment key={v.keyPrefix}>
+          <List {...v} />
+          <Divider sx={{ marginTop: 1, marginBottom: 1 }} />
+        </Fragment>
+      ))}
       <Stack direction="row" columnGap={2}>
         <Button
           variant="outlined"
