fe6e3f3cbc4c76f99fc5a5e64f5778c91eeedcec
linnefromice <linnefromice@gmail.com>
2022-03-10 15:55:30 +0900

    add new route to new form

---
+++ src/pages/paidMemberMng/matterForm/create_chiraceo.tsx
@@ -0,0 +1,8 @@
+import { CreateChiraceoScreen } from "components/screens/paidMemberMng/matterForm/create_chiraceo";
+import { NextPage } from "next";
+
+const PaidMemberMngMatterFormCreateChiraceo: NextPage = () => (
+  <CreateChiraceoScreen />
+);
+
+export default PaidMemberMngMatterFormCreateChiraceo;
