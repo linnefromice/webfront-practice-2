2b4eb7d77afd421887746dfbff0ed8bbcbe83a02
linnefromice <linnefromice@gmail.com>
2022-03-11 12:00:09 +0900

    refactor - way to create input in new form & update eslint rule

--- src/components/screens/paidMemberMng/matterForm/create_chiraceo/Form/index.tsx
+++ src/components/screens/paidMemberMng/matterForm/create_chiraceo/Form/index.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "@mui/material";
 import { RadioGroup, TextField } from "components/uiParts";
+import { RadioGroupProps } from "components/uiParts/RadioGroup";
+import { TextFieldProps } from "components/uiParts/TextField";
 import { useCommonHooks } from "libs/utils";
-import { VFC } from "react";
+import { Fragment, VFC } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import {
   AVERAGE_LEAD_TIME_MONTH,
@@ -28,9 +30,162 @@ import {
   VALUES_SOUGHT_LABELS,
 } from "../types";
 
+type CommonCreateInputArgs = { formDataKey: keyof FormData; label?: string };
+type ArgsForTextField = {
+  type: "textField";
+} & CommonCreateInputArgs &
+  Omit<TextFieldProps, "formDataKey" | "label">;
+type ArgsForRadioGroup = {
+  type: "radioGroup";
+  id?: string;
+} & CommonCreateInputArgs &
+  Omit<RadioGroupProps, "formDataKey" | "label" | "id">;
+type CreateInputArgs = ArgsForTextField | ArgsForRadioGroup;
+
+const isTextField = (v: CreateInputArgs): v is ArgsForTextField =>
+  v.type === "textField";
+const isRadioGroup = (v: CreateInputArgs): v is ArgsForRadioGroup =>
+  v.type === "radioGroup";
+
+const createInput = (args: ArgsForTextField | ArgsForRadioGroup) => {
+  if (isTextField(args)) {
+    const { type: _, formDataKey, label, ...rest } = args;
+    return (
+      <TextField
+        formDataKey={formDataKey}
+        label={label ? label : FormDataLabels[formDataKey]}
+        {...rest}
+      />
+    );
+  }
+
+  if (isRadioGroup(args)) {
+    const { type: _, formDataKey, label, id, ...rest } = args;
+    return (
+      <RadioGroup
+        formDataKey={formDataKey}
+        label={label ? label : FormDataLabels[formDataKey]}
+        id={id ? id : formDataKey}
+        {...rest}
+      />
+    );
+  }
+  return <></>;
+};
+
+const createInputComponentArgs = (isMobile: boolean): CreateInputArgs[] => [
+  { type: "textField", formDataKey: "hubspotId" },
+  { type: "textField", formDataKey: "otsunagiManager" },
+  { type: "textField", formDataKey: "otsunagiMailAddress" },
+  { type: "textField", formDataKey: "otsunagiManagerTelNumber" },
+  {
+    type: "radioGroup",
+    formDataKey: "otsunagiManagerRole",
+    choices: MANAGER_ROLES.map((v) => ({
+      label: MANAGER_ROLE_LABELS[v],
+      value: v,
+    })),
+    direction: "column",
+  },
+  {
+    type: "radioGroup",
+    formDataKey: "contactMethodWithOtsunagiManager",
+    choices: CONTACT_METHODS.map((v) => ({ label: v, value: v })),
+    direction: isMobile ? "column" : "row",
+  },
+  { type: "textField", formDataKey: "operationStartDate" },
+  { type: "textField", formDataKey: "contractEndDate" },
+  {
+    type: "radioGroup",
+    formDataKey: "plan",
+    choices: PLANS.map((v) => ({ label: PLAN_LABELS[v], value: v })),
+    direction: "column",
+  },
+  {
+    type: "radioGroup",
+    formDataKey: "needsInIntroduction",
+    choices: NEEDS.map((v) => ({ label: NEEDS_LABELS[v], value: v })),
+    direction: "column",
+  },
+  { type: "textField", formDataKey: "introductionBackground" },
+  {
+    type: "radioGroup",
+    formDataKey: "valueSought",
+    choices: VALUES_SOUGHT.map((v) => ({
+      label: VALUES_SOUGHT_LABELS[v],
+      value: v,
+    })),
+    direction: "column",
+  },
+  { type: "textField", formDataKey: "kpiMonthlyAppointments" },
+  { type: "textField", formDataKey: "averageMonthlyUnitPrice" },
+  { type: "textField", formDataKey: "averageNumberOfContractMonths" },
+  {
+    type: "radioGroup",
+    formDataKey: "averageLeadTimeMonth",
+    choices: AVERAGE_LEAD_TIME_MONTH.map((v) => ({
+      label: AVERAGE_LEAD_TIME_MONTH_LABELS[v],
+      value: v,
+    })),
+    direction: isMobile ? "column" : "row",
+  },
+  { type: "textField", formDataKey: "idealProductTarget" },
+  {
+    type: "radioGroup",
+    formDataKey: "enableToBusinessAllianceAppointment",
+    choices: STATUS.map((v) => ({ label: STATUS_LABELS[v], value: v })),
+    direction: "row",
+  },
+  {
+    type: "radioGroup",
+    formDataKey: "serviceThatCannotReceiveMutualProposal",
+    choices: PRODUCT_CATEGORIES.map((v) => ({
+      label: PRODUCT_CATEGORIES_LABELS[v],
+      value: v,
+    })),
+    direction: "column",
+  },
+  {
+    type: "radioGroup",
+    formDataKey: "resourceStatusInThreeMonth",
+    choices: RESOURCE_STATUS.map((v) => ({ label: v, value: v })),
+    direction: "row",
+  },
+  { type: "textField", formDataKey: "frontManager" },
+  {
+    type: "radioGroup",
+    formDataKey: "employeeSize",
+    choices: EMPLOYEE_SIZES.map((v) => ({
+      label: EMPLOYEE_SIZES_LABELS[v],
+      value: v,
+    })),
+    direction: "column",
+  },
+  {
+    type: "radioGroup",
+    formDataKey: "industry",
+    choices: INDUSTRIES.map((v) => ({ label: INDUSTRIES_LABELS[v], value: v })),
+    direction: "column",
+  },
+  {
+    type: "radioGroup",
+    formDataKey: "productCategory",
+    choices: PRODUCT_CATEGORIES.map((v) => ({
+      label: PRODUCT_CATEGORIES_LABELS[v],
+      value: v,
+    })),
+    direction: "column",
+  },
+  { type: "textField", formDataKey: "serviceName" },
+  { type: "textField", formDataKey: "serviceUrl" },
+  { type: "textField", formDataKey: "introduction" },
+];
+
 export const Contents: VFC = () => {
   const methods = useForm<FormData>();
   const { isMobile } = useCommonHooks();
+  const inputComponentArgs = createInputComponentArgs(isMobile);
+
   const onSubmit = (v: FormData) => console.log(v); // Temporary
   const onError = (e: any) => console.log(e); // Temporary
 
@@ -38,183 +193,9 @@ export const Contents: VFC = () => {
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit, onError)}>
         <Stack spacing={2}>
-          <TextField formDataKey="hubspotId" label={FormDataLabels.hubspotId} />
-          <TextField
-            formDataKey="otsunagiManager"
-            label={FormDataLabels.otsunagiManager}
-          />
-          <TextField
-            formDataKey="otsunagiMailAddress"
-            label={FormDataLabels.otsunagiMailAddress}
-          />
-          <TextField
-            formDataKey="otsunagiManagerTelNumber"
-            label={FormDataLabels.otsunagiManagerTelNumber}
-          />
-          <RadioGroup
-            formDataKey={`otsunagiManagerRole`}
-            label={FormDataLabels.otsunagiManagerRole}
-            id={`otsunagiManagerRole`}
-            choices={MANAGER_ROLES.map((v) => ({
-              label: MANAGER_ROLE_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <RadioGroup
-            formDataKey={`contactMethodWithOtsunagiManager`}
-            label={FormDataLabels.contactMethodWithOtsunagiManager}
-            id={`contactMethodWithOtsunagiManager`}
-            choices={CONTACT_METHODS.map((v) => ({
-              label: v,
-              value: v,
-            }))}
-            direction={isMobile ? "column" : "row"}
-          />
-          <TextField
-            formDataKey="operationStartDate"
-            label={FormDataLabels.operationStartDate}
-          />
-          <TextField
-            formDataKey="contractEndDate"
-            label={FormDataLabels.contractEndDate}
-          />
-          <RadioGroup
-            formDataKey={`plan`}
-            label={FormDataLabels.plan}
-            id={`plan`}
-            choices={PLANS.map((v) => ({
-              label: PLAN_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <RadioGroup
-            formDataKey={`needsInIntroduction`}
-            label={FormDataLabels.needsInIntroduction}
-            id={`needsInIntroduction`}
-            choices={NEEDS.map((v) => ({
-              label: NEEDS_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <TextField
-            formDataKey="introductionBackground"
-            label={FormDataLabels.introductionBackground}
-          />
-          <RadioGroup
-            formDataKey={`valueSought`}
-            label={FormDataLabels.valueSought}
-            id={`valueSought`}
-            choices={VALUES_SOUGHT.map((v) => ({
-              label: VALUES_SOUGHT_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <TextField
-            formDataKey="kpiMonthlyAppointments"
-            label={FormDataLabels.kpiMonthlyAppointments}
-          />
-          <TextField
-            formDataKey="averageMonthlyUnitPrice"
-            label={FormDataLabels.kpiMonthlyAppointments}
-          />
-          <TextField
-            formDataKey="averageNumberOfContractMonths"
-            label={FormDataLabels.kpiMonthlyAppointments}
-          />
-          <RadioGroup
-            formDataKey={`averageLeadTimeMonth`}
-            label={FormDataLabels.valueSought}
-            id={`averageLeadTimeMonth`}
-            choices={AVERAGE_LEAD_TIME_MONTH.map((v) => ({
-              label: AVERAGE_LEAD_TIME_MONTH_LABELS[v],
-              value: v,
-            }))}
-            direction={isMobile ? "column" : "row"}
-          />
-          <TextField
-            formDataKey="idealProductTarget"
-            label={FormDataLabels.idealProductTarget}
-          />
-          <RadioGroup
-            formDataKey={`enableToBusinessAllianceAppointment`}
-            label={FormDataLabels.enableToBusinessAllianceAppointment}
-            id={`enableToBusinessAllianceAppointment`}
-            choices={STATUS.map((v) => ({
-              label: STATUS_LABELS[v],
-              value: v,
-            }))}
-            direction={"row"}
-          />
-          <RadioGroup
-            formDataKey={`serviceThatCannotReceiveMutualProposal`}
-            label={FormDataLabels.serviceThatCannotReceiveMutualProposal}
-            id={`serviceThatCannotReceiveMutualProposal`}
-            choices={PRODUCT_CATEGORIES.map((v) => ({
-              label: PRODUCT_CATEGORIES_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <RadioGroup
-            formDataKey={`resourceStatusInThreeMonth`}
-            label={FormDataLabels.resourceStatusInThreeMonth}
-            id={`resourceStatusInThreeMonth`}
-            choices={RESOURCE_STATUS.map((v) => ({
-              label: v,
-              value: v,
-            }))}
-            direction={"row"}
-          />
-          <TextField
-            formDataKey="frontManager"
-            label={FormDataLabels.frontManager}
-          />
-          <RadioGroup
-            formDataKey={`employeeSize`}
-            label={FormDataLabels.employeeSize}
-            id={`employeeSize`}
-            choices={EMPLOYEE_SIZES.map((v) => ({
-              label: EMPLOYEE_SIZES_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <RadioGroup
-            formDataKey={`industry`}
-            label={FormDataLabels.industry}
-            id={`industry`}
-            choices={INDUSTRIES.map((v) => ({
-              label: INDUSTRIES_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <RadioGroup
-            formDataKey={`productCategory`}
-            label={FormDataLabels.productCategory}
-            id={`productCategory`}
-            choices={PRODUCT_CATEGORIES.map((v) => ({
-              label: PRODUCT_CATEGORIES_LABELS[v],
-              value: v,
-            }))}
-            direction={"column"}
-          />
-          <TextField
-            formDataKey="serviceName"
-            label={FormDataLabels.serviceName}
-          />
-          <TextField
-            formDataKey="serviceUrl"
-            label={FormDataLabels.serviceUrl}
-          />
-          <TextField
-            formDataKey="introduction"
-            label={FormDataLabels.introduction}
-          />
+          {inputComponentArgs.map((v) => (
+            <Fragment key={v.formDataKey}>{createInput(v)}</Fragment>
+          ))}
         </Stack>
       </form>
     </FormProvider>
