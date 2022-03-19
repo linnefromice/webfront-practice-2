b38b2e5fedea5a72fc04ada9411df1576778840b
linnefromice <linnefromice@gmail.com>
2022-03-19 22:50:55 +0900

    refactor

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,22 +1,3 @@
-import faker from "@faker-js/faker";
-import { REQUIRED_FORM_ITEM_VALUES } from "components/screens/paidMemberMng/consulting/create/mocks";
-import { graphql } from "msw";
+import { GetConsultingForms } from "./queries/GetConsultingForms";
 
-export const handlers = [
-  graphql.query("GetConsultingForms", (req, res, ctx) => {
-    return res(
-      ctx.data({
-        forms: [...Array(100)].map((_, i) => ({
-          ...REQUIRED_FORM_ITEM_VALUES,
-          hubspotId: `${i + 1}`,
-          otsunagiManager: faker.name.findName(),
-          otsunagiMailAddress: faker.internet.email(),
-          otsunagiManagerTelNumber:
-            Math.random() > 0.5
-              ? faker.phone.phoneNumber("03-####-####")
-              : faker.phone.phoneNumber("0##-###-####"),
-        })),
-      })
-    );
-  }),
-];
+export const handlers = [GetConsultingForms];
---
+++ src/mocks/queries/GetConsultingForms.ts
@@ -0,0 +1,23 @@
+import faker from "@faker-js/faker";
+import { REQUIRED_FORM_ITEM_VALUES } from "components/screens/paidMemberMng/consulting/create/mocks";
+import { graphql } from "msw";
+
+export const GetConsultingForms = graphql.query(
+  "GetConsultingForms",
+  (req, res, ctx) => {
+    return res(
+      ctx.data({
+        forms: [...Array(100)].map((_, i) => ({
+          ...REQUIRED_FORM_ITEM_VALUES,
+          hubspotId: `${i + 1}`,
+          otsunagiManager: faker.name.findName(),
+          otsunagiMailAddress: faker.internet.email(),
+          otsunagiManagerTelNumber:
+            Math.random() > 0.5
+              ? faker.phone.phoneNumber("03-####-####")
+              : faker.phone.phoneNumber("0##-###-####"),
+        })),
+      })
+    );
+  }
+);
